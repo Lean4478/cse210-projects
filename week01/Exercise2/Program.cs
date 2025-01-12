9ef532ed1fde33c80950b19a1936d42f8966f1fa
Leandro <leandrolopezsud2017@gmail.com>
2025-01-11 23:29:22 -0300

    corrections

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Please, enter your grade percentage: ");
+
+        int percentage = int.Parse(Console.ReadLine());
+
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+           letterGrade = "F"; 
+        }
+
+        Console.Write($"Your letter grade is: {letterGrade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratularion!! You passed the class");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Better luck next time!");
+        }
+
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+
+        int magicNumber = random.Next(1,101);
+
+        int userGuess;
+
+        Console.WriteLine("Guess a magic number between 1 and 100: ");
+
+        do
+        
+        {
+
+            Console.Write("Your guess: ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Too low! Higher");
+            }
+
+            else if (userGuess > magicNumber)
+
+            {
+                Console.WriteLine("Too high! Lower");
+            }
+
+            else 
+            {
+                Console.WriteLine("You guessed it");
+            }
+        } while (userGuess != magicNumber);
+
     }
 }
