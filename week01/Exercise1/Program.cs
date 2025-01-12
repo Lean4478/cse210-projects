6b4979ae52914592298308a43cc2a444fe9178c2
Leandro <leandrolopezsud2017@gmail.com>
2025-01-11 23:10:22 -0300

    "exersices of week 1"

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,97 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+        /*Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        //TRYING C#
+        /*Console.Write("What is your favorite color?");
+        tring color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}");/*
+
+        //1st ACTIVITY
+        /*Console.Write("What is your name?");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}");
+
+        //2nd ACTIVITY
+        /*Console.Write("Please, enter your grade percentage: ");
+
+        int percentage = int.Parse(Console.ReadLine());
+
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+           letterGrade = "F"; 
+        }
+
+        Console.Write($"Your letter grade is: {letterGrade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratularion!! You passed the class");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Better luck next time!");
+        }/**/
+
+
+        //3rd ACTIVITY
+        Random random = new Random();
+
+        int magicNumber = random.Next(1,101);
+
+        int userGuess;
+
+        Console.WriteLine("Guess a magic number between 1 and 100: ");
+
+        do
+        
+        {
+
+            Console.Write("Your guess: ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Too low! Higher");
+            }
+
+            else if (userGuess > magicNumber)
+
+            {
+                Console.WriteLine("Too high! Lower");
+            }
+
+            else 
+            {
+                Console.WriteLine("You guessed it");
+            }
+        } while (userGuess != magicNumber);
+    
+    }      
 }
